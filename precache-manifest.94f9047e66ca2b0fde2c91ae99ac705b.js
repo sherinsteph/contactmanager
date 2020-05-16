e81f5ad8ec9fd029ee104073aef9ddc9afa5c1d0
Sherin Stephen <sherinstephen005@gmail.com>
2020-05-16 22:22:09 +0530

    Updates

---
+++ precache-manifest.94f9047e66ca2b0fde2c91ae99ac705b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac8b9566a2258c96bc70f84fdd9dd6a4",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "32befc4213ce2b5c115f",
+    "url": "/contactmanager/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "e5ed454c720cafeebf79",
+    "url": "/contactmanager/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "32befc4213ce2b5c115f",
+    "url": "/contactmanager/static/js/2.8a910134.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.8a910134.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5ed454c720cafeebf79",
+    "url": "/contactmanager/static/js/main.096a9f9f.chunk.js"
+  },
+  {
+    "revision": "3959f31844f85f026a86",
+    "url": "/contactmanager/static/js/runtime-main.711199b3.js"
+  }
+]);
